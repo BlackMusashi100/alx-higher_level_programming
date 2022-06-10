2b30470f282a9932db27bbbb7c6fa222e3c16d7f
Shaban Haruna <shabanharuna100@gmail.com>
2022-06-10 12:50:27 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,10 @@
-ls
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+matrix = [[0,1,2],[3,4,5],[6,7,8]]
+
+matrix = [x**2 for x in range(3)]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
